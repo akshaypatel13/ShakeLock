9d5465c61c98e10e2035abe74eb1d446f8a81b0c
sagarmoghe5 <16164018+sagarmoghe5@users.noreply.github.com>
2020-04-02 16:47:33 -0300

    need shared preference uri list

--- app/src/main/java/com/example/myapplication/Dashboard.kt
+++ app/src/main/java/com/example/myapplication/Dashboard.kt
@@ -1,13 +1,18 @@
 package com.example.myapplication
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import androidx.biometric.BiometricPrompt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
+import com.google.gson.Gson
 import com.kotlinpermissions.KotlinPermissions
 import kotlinx.android.synthetic.main.activity_dashboard.*
 import kotlinx.android.synthetic.main.activity_main.gifImageView
@@ -18,10 +23,19 @@ import java.util.concurrent.Executors
 
 class Dashboard : AppCompatActivity() {
 
+    private lateinit var mShakeDetector: ShakeDetector
+    private lateinit var mSensorManager: SensorManager
+    private lateinit var mAccelerometer: Sensor
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         KotlinPermissions.with(this) // where this is an FragmentActivity instance
-            .permissions(Manifest.permission.USE_BIOMETRIC,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            .permissions(
+                Manifest.permission.USE_BIOMETRIC,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
             .onAccepted { permissions ->
             }
             .onDenied { permissions ->
@@ -33,32 +47,32 @@ class Dashboard : AppCompatActivity() {
             .ask()
         setContentView(R.layout.activity_dashboard)
 
-        try{
-            var inp_st: InputStream =assets.open("gif1.gif")
-            var bts:ByteArray= IOUtils.toByteArray(inp_st)
+        try {
+            var inp_st: InputStream = assets.open("gif1.gif")
+            var bts: ByteArray = IOUtils.toByteArray(inp_st)
             gifImageView.setBytes(bts)
             gifImageView.startAnimation()
-        }catch(exc: IOException){
+        } catch (exc: IOException) {
         }
 
         //---------------SharedPreferences----------------
 
-        val check_user:SharedPreferences=this.getSharedPreferences("user_log",0)
-        var user_inp=check_user.getString("user_pattern","0")
-        var user_pin_inp=check_user.getString("user_pin","0")
-        var user_password_inp=check_user.getString("user_password","0")
-        var set_secirity_inp = check_user.getString("user_security","0")
+        val check_user: SharedPreferences = this.getSharedPreferences("user_log", 0)
+        var user_inp = check_user.getString("user_pattern", "0")
+        var user_pin_inp = check_user.getString("user_pin", "0")
+        var user_password_inp = check_user.getString("user_password", "0")
+        var set_secirity_inp = check_user.getString("user_security", "0")
         //------------------Pattern-----------------------
 
         btn_pattern.setOnClickListener {
 
-            if(user_inp.equals("0")){
+            if (user_inp.equals("0")) {
 
-                val intent=Intent(this, SetupPattern::class.java)
+                val intent = Intent(this, SetupPattern::class.java)
                 startActivity(intent)
-            }else if(user_inp.equals("1")) {
+            } else if (user_inp.equals("1")) {
 
-                val intent=Intent(this, InputPattern::class.java)
+                val intent = Intent(this, InputPattern::class.java)
                 startActivity(intent)
             }
 
@@ -68,25 +82,26 @@ class Dashboard : AppCompatActivity() {
         //------------------FingerPrint------------------------
 
         val exec = Executors.newSingleThreadExecutor()
-        val current_act:FragmentActivity=this
+        val current_act: FragmentActivity = this
 
-        val finger_prompt=BiometricPrompt(current_act,exec,object :BiometricPrompt.AuthenticationCallback() {
-            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-                super.onAuthenticationSucceeded(result)
-                val intent=Intent(applicationContext, Home::class.java)
-                startActivity(intent)
-            }
+        val finger_prompt =
+            BiometricPrompt(current_act, exec, object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    val intent = Intent(applicationContext, Home::class.java)
+                    startActivity(intent)
+                }
 
-            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-                super.onAuthenticationError(errorCode, errString)
-            }
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                }
 
-            override fun onAuthenticationFailed() {
-                super.onAuthenticationFailed()
-            }
-        })
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                }
+            })
 
-        val info=BiometricPrompt.PromptInfo.Builder()
+        val info = BiometricPrompt.PromptInfo.Builder()
             .setNegativeButtonText("a")
             .setDescription("aa")
             .setTitle("FingerPrint")
@@ -101,12 +116,11 @@ class Dashboard : AppCompatActivity() {
         //---------------------Pin---------------------
 
         btn_pin.setOnClickListener {
-            if(user_pin_inp.equals("0")){
-                val intent=Intent(applicationContext, SetupPin::class.java)
+            if (user_pin_inp.equals("0")) {
+                val intent = Intent(applicationContext, SetupPin::class.java)
                 startActivity(intent)
-            }
-            else if(user_pin_inp.equals("1")){
-                val intent=Intent(applicationContext, InputPin::class.java)
+            } else if (user_pin_inp.equals("1")) {
+                val intent = Intent(applicationContext, InputPin::class.java)
                 startActivity(intent)
             }
 
@@ -117,10 +131,10 @@ class Dashboard : AppCompatActivity() {
 
 
         btn_password.setOnClickListener {
-            if(user_password_inp.equals("0")) {
+            if (user_password_inp.equals("0")) {
                 val intent = Intent(applicationContext, SetupPassword::class.java)
                 startActivity(intent)
-            }else{
+            } else {
                 val intent = Intent(applicationContext, InputPassword::class.java)
                 startActivity(intent)
             }
@@ -128,12 +142,49 @@ class Dashboard : AppCompatActivity() {
 
         //--------------------Set Security Key------------------
         set_security_key.setOnClickListener {
-            val intent = Intent(applicationContext,SetSecurityKey::class.java)
+            val intent = Intent(applicationContext, SetSecurityKey::class.java)
             startActivity(intent)
         }
 
+        // Shake Detector
+        //
+        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        mShakeDetector = ShakeDetector(object : ShakeDetector.OnShakeListener {
+            override fun onShake() {
+                Log.d("Shaken", "check reached")
+                val check: SharedPreferences = getSharedPreferences("shared_uris", 0)
+                val gson1 = Gson()
+                var ur = check.getString("urri", null)
+//                Log.i("aa", ur)
+
+                Toast.makeText(applicationContext, ur, Toast.LENGTH_LONG).show()
+                //  Toast.makeText(applicationContext,ur,Toast.LENGTH_LONG).show()
+//                var json: String? = sp.getString("SHakeDetectTAG", null)
+                //              val type: Type = object : TypeToken<ArrayList<String?>?>() {}.type
+                //var fileList = gson.fromJson<>(json,type)
+                //            Log.d("json sp",sp.toString())
+            }
+        })
+        //
+        //--
+
+
     }
+
     override fun onBackPressed() {
 
     }
+
+    override fun onResume() {
+        super.onResume()
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI)
+    }
+    override fun onPause() {
+        mSensorManager.unregisterListener(mShakeDetector)
+        super.onPause()
+    }
+
 }
+
+
--- app/src/main/java/com/example/myapplication/Home.kt
+++ app/src/main/java/com/example/myapplication/Home.kt
@@ -1,58 +1,141 @@
 package com.example.myapplication
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
+import androidx.annotation.RequiresApi
+import androidx.core.net.toUri
 import co.csadev.kwikpicker.KwikPicker
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.*
 import kotlinx.android.synthetic.main.activity_home.*
+import com.github.tbouron.shakedetector.library.ShakeDetector
 
 class Home : AppCompatActivity() {
     var uris: ArrayList<Uri>? = null
+    var txt_pathShow: TextView? = null
+    var btn_filePicker: Button? = null
+    var myFileIntent: Intent? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        pick.setOnClickListener {
-            val kwikPicker = KwikPicker.Builder(applicationContext,
-                imageProvider = { imageView, uri ->
-                    Glide.with(this)
-                        .load(uri)
-                        .into(imageView)
-                },
-                onMultiImageSelectedListener = { list: ArrayList<Uri> ->
-                    add_to_List(list)
-                },
-                peekHeight = 1600,
-                showTitle = false,
-                completeButtonText = "Done",
-                emptySelectionText = "No Selection")
-                .create(applicationContext)
-            kwikPicker.show(supportFragmentManager)
+
+        ShakeDetector.create(
+            this
+        ) { Toast.makeText(applicationContext, "Device shaken!", Toast.LENGTH_SHORT).show()
+        Log.d("test Shake", "shaker")}
+
+
+
+
+        txt_pathShow = findViewById<View>(R.id.txt_path) as TextView
+        btn_filePicker = findViewById<View>(R.id.btn_filePicker) as Button
+        btn_filePicker!!.setOnClickListener {
+            val intent = Intent()
+                .setType("*/*")
+                .setAction(Intent.ACTION_GET_CONTENT)
+                .putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            startActivityForResult(Intent.createChooser(intent, "Select a file"), 111)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d("abb", data.toString())
+        Log.d("abb", " ")
+        Toast.makeText(applicationContext, data.toString(), Toast.LENGTH_LONG).show()
+
+        val data2= ArrayList<Uri>()
+        if (requestCode == 111) {
+            if (resultCode == Activity.RESULT_OK) {
+                //multiple
+                if (data?.getClipData() != null) {
+
+                    var count = data.getClipData()!!.getItemCount()
+                    var currentItem = 0
+                    while (currentItem < count) {
+                        var imageUri: Uri = data.getClipData()!!.getItemAt(currentItem).getUri()
+                        //do something with the image (save it to some directory or whatever you need to do with it here)
+                        var demo = imageUri.getPath().toUri().toString()
+                        demo = demo.substring(demo.indexOf(":") + 1)
+
+                        data2.add(demo.toUri())
+                        currentItem = currentItem + 1;
+                        Log.d("abb", data2.toString())
+                        add_to_List(data2)
+                        txt_pathShow!!.text = data2.toString()
+                    }
+                //single
+                } else if (data?.getData() != null) {
+
+                    val FilePath = data.getData()!!.getPath()
+                    var demo =FilePath?.toUri()!!.toString()
+                    demo = demo.substring(demo.indexOf(":") + 1)
+
+                    data2.add(demo.toUri())
+                    Log.d("abb", data2.toString())
+                    add_to_List(data2)
+                    txt_pathShow!!.text = data2.toString()
+                }
+
+            }
 
         }
-
-
-
     }
+
+        //btn_filePicker.setOnClickListener {
+//            val kwikPicker = KwikPicker.Builder(applicationContext,
+//                imageProvider = { imageView, uri ->
+//                    Glide.with(this)
+//                        .load(uri)
+//                        .into(imageView)
+//                },
+//                onMultiImageSelectedListener = { list: ArrayList<Uri> ->
+//                    add_to_List(list)
+//                },
+//                peekHeight = 1600,
+//                showTitle = false,
+//                completeButtonText = "Done",
+//                emptySelectionText = "No Selection")
+//                .create(applicationContext)
+//            kwikPicker.show(supportFragmentManager)
+//        }
+//    }
 
     private fun add_to_List(list: ArrayList<Uri>) {
 
         uris=list
         Toast.makeText(applicationContext,uris.toString(),Toast.LENGTH_LONG).show()
 
+
     }
 
     override fun onBackPressed() {
         val intent = Intent(applicationContext,Dashboard::class.java)
         startActivity(intent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        ShakeDetector.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        ShakeDetector.stop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ShakeDetector.destroy()
     }
 
 }
